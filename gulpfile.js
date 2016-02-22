504262c111bf8416c5072ff674e1036fb8801ae6
Alif Rachmawadi <code@subosito.com>
2016-02-22 13:04:02 +0700

    added gulp watch

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var babel = require('gulp-babel');
+var path = require('path');
+var paths = { srcDir: ['src/**/*.js'], destDir: 'lib' };
+
+gulp.task('babel', function() {
+  return gulp.src(paths.srcDir).pipe(babel()).pipe(gulp.dest(paths.destDir));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(paths.srcDir, ['babel']);
+});
+
+gulp.task('default', ['watch']);
