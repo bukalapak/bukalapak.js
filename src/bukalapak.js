47e79df27c82609e97986e7601ececa650d8b5d8
Alif Rachmawadi <code@subosito.com>
2016-02-23 14:23:12 +0700

    added support for http methods

--- src/bukalapak.js
+++ src/bukalapak.js
@@ -1,10 +1,57 @@
+import fetch from 'isomorphic-fetch'
+
+const API_VERSION = 'v4'
+const METHODS = ['get', 'put', 'del', 'post', 'head', 'opts']
+
 class Bukalapak {
   constructor (options = {}) {
-    this.options = options
+    this.options = {}
+    this.headers = {
+      'Accept': `application/vnd.bukalapak.${API_VERSION}+json`
+    }
+
+    if (!options.baseUrl) {
+      throw new Error('`baseUrl` option is required')
+    } else {
+      this.options.baseUrl = options.baseUrl
+    }
+
+    METHODS.forEach((method) => {
+      this[method] = this._request(method)
+    })
   }
 
-  getOptions () {
-    return this.options
+  _request (method) {
+    return (path, options = {}) => {
+      if (typeof path !== 'string') { throw new Error('`path` must be a string') }
+      if (typeof options !== 'object' || Array.isArray(options)) { throw new Error('`options` must be an object') }
+
+      let opts = {
+        headers: {
+          ...this.headers
+        },
+        ...options,
+        method: this._methodMatcher(method)
+      }
+
+      // ensure body always present for POST request
+      if (opts.method === 'POST' && typeof opts.body === 'undefined') {
+        opts.body = ''
+      }
+
+      return this._fetch(this.options.baseUrl + path, opts)
+    }
+  }
+
+  _methodMatcher (method) {
+    if (method === 'del') { return 'DELETE' }
+    if (method === 'opts') { return 'OPTIONS' }
+
+    return method.toUpperCase()
+  }
+
+  _fetch (...args) {
+    return fetch(...args)
   }
 }
 
--- test/test.js
+++ test/test.js
@@ -1,18 +1,46 @@
+/*eslint no-new: 0*/
+
 import { describe, it } from 'mocha'
 import { expect } from 'chai'
 import Bukalapak from '../src/bukalapak'
 
-describe('Bukalapak', () => {
-  it('should creates a new instance', () => {
-    let options = { clientId: '12345', clientSecret: 's3cr3t' }
-    let bukalapak = new Bukalapak(options)
+let baseUrl = 'https://api.bukalapak.com'
+let options = { baseUrl: baseUrl }
 
+describe('Bukalapak', () => {
+  it('should create a new instance', () => {
+    let bukalapak = new Bukalapak(options)
     expect(bukalapak).to.be.an.instanceOf(Bukalapak)
-    expect(bukalapak.getOptions()).to.equal(options)
+  })
+
+  it('should raise error when no baseUrl option', () => {
+    expect(() => { new Bukalapak({}) }).to.throw(Error, '`baseUrl` option is required')
   })
 
   it('should raise error when called as function', () => {
-    expect(() => { Bukalapak() }).to.throw(TypeError)
+    expect(() => { Bukalapak(options) }).to.throw(TypeError, 'Cannot call a class as a function')
+  })
+
+  it('should format options and remove invalid keys', () => {
+    let bukalapak = new Bukalapak(Object.assign(options, { invalidKey: 'foo' }))
+    expect(bukalapak.options).to.eql({ baseUrl: baseUrl })
+  })
+
+  describe('http methods', () => {
+    let bukalapak = new Bukalapak(options)
+
+    it('should support all http methods', () => {
+      let methods = ['get', 'put', 'del', 'post', 'head', 'opts']
+      methods.forEach((method) => { expect(bukalapak[method]).to.be.a('function') })
+    })
+
+    it('should throw an error if path is not a string', () => {
+      expect(() => { bukalapak.get({}) }).to.throw(Error, '`path` must be a string')
+    })
+
+    it('should throw an error if options is not an object', () => {
+      expect(() => { bukalapak.get('', []) }).to.throw(Error, '`options` must be an object')
+      expect(() => { bukalapak.get('', 11) }).to.throw(Error, '`options` must be an object')
+    })
   })
 })
-
